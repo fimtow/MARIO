f3db965d3f92b208bd3d3108f067ba6dc330cb6d
samia <chqirsamia@gmail.com>
2021-06-03 15:29:59 +0100

    update

--- src/com/TETOSOFT/test/Background.java
+++ src/com/TETOSOFT/test/Background.java
@@ -20,17 +20,17 @@ public class Background extends JPanel {
 	
 
 	 public void paint(Graphics g){
-		    Image img1 = Toolkit.getDefaultToolkit().getImage("images/background1.jpg");
+		    Image img1 = Toolkit.getDefaultToolkit().getImage("images/backgrounds1.jpg");
 		    g.drawImage(img1, 10, 100, this);
 		    
-		    Image img2 = Toolkit.getDefaultToolkit().getImage("images/background2.jpg");
-		    g.drawImage(img2, 900, 300, this);
+		    Image img2 = Toolkit.getDefaultToolkit().getImage("images/backgrounds2.jpg");
+		    g.drawImage(img2, 900, 100, this);
 		    
-		    Image img3 = Toolkit.getDefaultToolkit().getImage("images/background3.jpg");
-		    g.drawImage(img3, 10, 300, this);
+		    Image img3 = Toolkit.getDefaultToolkit().getImage("images/backgrounds3.jpg");
+		    g.drawImage(img3, 10, 350, this);
 		    
-		    Image img4 = Toolkit.getDefaultToolkit().getImage("images/background4.jpg");
-		    g.drawImage(img4, 900, 100, this);
+		    Image img4 = Toolkit.getDefaultToolkit().getImage("images/backgrounds4.jpg");
+		    g.drawImage(img4, 900, 350, this);
 		    g.drawString("fermer la fenetre apres avoir choisi votre numero de background", 400, 400);
 		  }
 }
